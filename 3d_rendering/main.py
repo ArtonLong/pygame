248d84cf54a487b4fe0ce6c1aeabff45c3ee102f
ArtonLong <artonlong@gmail.com>
2026-01-28 09:53:30 +0100

    walls and floors, stretching big when looking at walls from the side

--- 3d_rendering/main.py
+++ 3d_rendering/main.py
@@ -16,7 +16,7 @@ class App:
 
         self.player = Player(70, -110, 20, 0, 0)
 
-        self.sectors = [Sector(0, 4, 0, 40), Sector(4, 8, 0, 40)]
+        self.sectors = [Sector(0, 4, 0, 40, 3, 4), Sector(4, 8, 0, 40, 1, 2)]
         self.walls = [
             Wall(0,0,32,0,1), Wall(32,0,32,32,2), Wall(32,32,0,32,1), Wall(0,32,0,0,2),
             Wall(64,96,0,0,3), Wall(96,96,0,32,4), Wall(96,64,32,32,3), Wall(64,64,32,0,4)
@@ -42,7 +42,7 @@ class App:
         z1 = z1 + s * (z2 - z1)
         return x1, y1, z1
 
-    def draw_wall(self, x1, x2, b1, b2, t1, t2, c):
+    def draw_wall(self, x1, x2, b1, b2, t1, t2, c, s: Sector):
         dyb = b2 - b1
         dyt = t2 - t1
         dx = x2 - x1
@@ -55,13 +55,32 @@ class App:
         if x1 > WIDTH-1: x1 = WIDTH-1
         if x2 > WIDTH-1: x2 = WIDTH-1
 
-        x = x1
+        x = math.floor(x1)-1
+
         while x<x2:
+            x+=1
             y1 = dyb*(x-xs+0.5)/dx+b1
             y2 = dyt*(x-xs+0.5)/dx+t1
+            
+            if s.surface == 1: 
+                
+                s.surf[x] = y1
+                continue
+            if s.surface == 2: 
+                s.surf[x] = y2
+                continue
+            if s.surface == -1:
+                y=s.surf[x]
+                while y<y1:
+                    self.draw_pixel(x,y,s.cb)
+                    y+=1
+            if s.surface == -2:
+                y=y2
+                while y<s.surf[x]:
+                    self.draw_pixel(x,y,s.ct)
+                    y+=1
             y=y1
             while y<y2:
-
                 if y1 < 1: y1 = 1
                 if y2 < 1: y2 = 1
                 if y1 > HEIGHT-1: y1 = HEIGHT-1
@@ -69,7 +88,6 @@ class App:
 
                 self.draw_pixel(x,y,c)
                 y+=1
-            x+=1
 
     def draw_3d(self):
         wx = [0,0,0,0]
@@ -87,57 +105,73 @@ class App:
         #offsetting the position of the wall point 1 at 40, 10. by the player
         for s in self.sectors:
             s.d = 0
-            for i in range(s.ws, s.we):
-                w = self.walls[i]
-            
-                x1 = w.x1 - self.player.x
-                y1 = w.y1 - self.player.y
-                x2 = w.x2 - self.player.x
-                y2 = w.y2 - self.player.y
+
+            if self.player.z < s.z1: s.surface = 1
+            elif self.player.z > s.z2: s.surface = 2
+            else: s.surface = 0
+
+            for loop in range(2):
+
+                for i in range(s.ws, s.we):
+                    w = self.walls[i]
                 
-                wx[0] = x1*cos-y1*sin
-                wx[1] = x2*cos-y2*sin
-                wx[2] = wx[0]
-                wx[3] = wx[1]
+                    x1 = w.x1 - self.player.x
+                    y1 = w.y1 - self.player.y
+                    x2 = w.x2 - self.player.x
+                    y2 = w.y2 - self.player.y
 
-                wy[0] = y1*cos+x1*sin
-                wy[1] = y2*cos+x2*sin
-                wy[2] = wy[0]
-                wy[3] = wy[1]
+                    if loop == 0:
+                        swp = x1
+                        x1 = x2
+                        x2 = swp
+                        swp = y1
+                        y1 = y2
+                        y2 = swp
+                    
+                    wx[0] = x1*cos-y1*sin
+                    wx[1] = x2*cos-y2*sin
+                    wx[2] = wx[0]
+                    wx[3] = wx[1]
 
-                s.d += self.dist(0, 0, (wx[0]+wx[1])/2, (wy[0]+wy[1])/2)
+                    wy[0] = y1*cos+x1*sin
+                    wy[1] = y2*cos+x2*sin
+                    wy[2] = wy[0]
+                    wy[3] = wy[1]
 
-                wz[0] = s.z1-self.player.z + ((self.player.l*wy[0])/32)
-                wz[1] = s.z1-self.player.z + ((self.player.l*wy[1])/32)
-                wz[2] = wz[0] + s.z2
-                wz[3] = wz[1] + s.z2
-                if wy[0] < 1 and wy[1] < 1: continue
+                    s.d += self.dist(0, 0, (wx[0]+wx[1])/2, (wy[0]+wy[1])/2)
 
-                if wy[0] < 1:
-                    wx[0], wy[0], wz[0] = self.clip_behind_player(wx[0], wy[0], wz[0], wx[1], wy[1], wz[1],)
-                    wx[2], wy[2], wz[2] = self.clip_behind_player(wx[2], wy[2], wz[2], wx[3], wy[3], wz[3],)
-                
-                if wy[1] < 1:
-                    wx[1], wy[1], wz[1] = self.clip_behind_player(wx[1], wy[1], wz[1], wx[0], wy[0], wz[0],)
-                    wx[3], wy[3], wz[3] = self.clip_behind_player(wx[3], wy[3], wz[3], wx[2], wy[2], wz[2],)
+                    wz[0] = s.z1-self.player.z + ((self.player.l*wy[0])/32)
+                    wz[1] = s.z1-self.player.z + ((self.player.l*wy[1])/32)
+                    wz[2] = wz[0] + s.z2
+                    wz[3] = wz[1] + s.z2
+                    if wy[0] < 1 and wy[1] < 1: continue
 
-                #screen pos
-                wx[0] = wx[0]*200/wy[0]+W2
-                wy[0] = wz[0]*200/wy[0]+H2
-                wx[1] = wx[1]*200/wy[1]+W2
-                wy[1] = wz[1]*200/wy[1]+H2
-                wx[2] = wx[2]*200/wy[2]+W2
-                wy[2] = wz[2]*200/wy[2]+H2
-                wx[3] = wx[3]*200/wy[3]+W2
-                wy[3] = wz[3]*200/wy[3]+H2
+                    if wy[0] < 1:
+                        wx[0], wy[0], wz[0] = self.clip_behind_player(wx[0], wy[0], wz[0], wx[1], wy[1], wz[1],)
+                        wx[2], wy[2], wz[2] = self.clip_behind_player(wx[2], wy[2], wz[2], wx[3], wy[3], wz[3],)
+                    
+                    if wy[1] < 1:
+                        wx[1], wy[1], wz[1] = self.clip_behind_player(wx[1], wy[1], wz[1], wx[0], wy[0], wz[0],)
+                        wx[3], wy[3], wz[3] = self.clip_behind_player(wx[3], wy[3], wz[3], wx[2], wy[2], wz[2],)
 
-                # self.draw_pixel(wx[0], wy[0], w.c)
-                # self.draw_pixel(wx[1], wy[1], w.c)
-                # self.draw_pixel(wx[2], wy[2], w.c)
-                # self.draw_pixel(wx[3], wy[3], w.c)
+                    #screen pos
+                    wx[0] = wx[0]*200/wy[0]+W2
+                    wy[0] = wz[0]*200/wy[0]+H2
+                    wx[1] = wx[1]*200/wy[1]+W2
+                    wy[1] = wz[1]*200/wy[1]+H2
+                    wx[2] = wx[2]*200/wy[2]+W2
+                    wy[2] = wz[2]*200/wy[2]+H2
+                    wx[3] = wx[3]*200/wy[3]+W2
+                    wy[3] = wz[3]*200/wy[3]+H2
 
-                self.draw_wall(wx[0], wx[1], wy[0], wy[1], wy[2], wy[3], w.c)
-            s.d /= (s.we-s.ws)
+                    # self.draw_pixel(wx[0], wy[0], w.c)
+                    # self.draw_pixel(wx[1], wy[1], w.c)
+                    # self.draw_pixel(wx[2], wy[2], w.c)
+                    # self.draw_pixel(wx[3], wy[3], w.c)
+
+                    self.draw_wall(wx[0], wx[1], wy[0], wy[1], wy[2], wy[3], w.c, s)
+                s.d /= (s.we-s.ws)
+                s.surface *= -1
 
     def draw_pixel(self, x, y, c):
         if c == 0: color = (255,255,255)
--- 3d_rendering/sector.py
+++ 3d_rendering/sector.py
@@ -1,4 +1,6 @@
 
+from settings import *
+
 class Wall:
     def __init__(self, x1, x2, y1, y2, c):
         self.x1, self.x2 = x1, x2
@@ -6,7 +8,10 @@ class Wall:
         self.c = c
 
 class Sector:
-    def __init__(self, ws, we, z1, z2):
+    def __init__(self, ws, we, z1, z2, ct, cb):
         self.ws, self.we = ws, we
         self.z1, self.z2 = z1, z2
+        self.ct, self.cb = ct, cb
         self.d = 0
+        self.surf = [0] * WIDTH
+        self.surface = 0
