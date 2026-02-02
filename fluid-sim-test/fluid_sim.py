2c385c2ca2f1ffc8da8a5b556529ed0a21f20b55
ArtonLong <artonlong@gmail.com>
2026-02-02 13:59:31 +0100

    no vectors in fluid sim to see if its faster

--- fluid-sim-test/fluid_sim.py
+++ fluid-sim-test/fluid_sim.py
@@ -16,8 +16,10 @@ class App:
         self.width, self.height = 800, 700
         self.size = (self.width, self.height)
 
-        self.bounds = pygame.math.Vector2(350, 350)
-        self.bounds_position = pygame.math.Vector2(100, 100)
+        self.bounds_width = 350
+        self.bounds_height = 350
+        self.bounds_x = 100
+        self.bounds_y = 100
         self.num_particles = 100
         self.spacing = 10
 
@@ -29,8 +31,8 @@ class App:
         self.smoothing_radius = 25
         self.gravity = 0.1
         self.MASS = 1
-        self.target_density = 130
-        self.pressure_multiplier = 460
+        self.target_density = 130 # 130
+        self.pressure_multiplier = 460 # 460
 
         self.particle_cells:dict[int, list[Particle]] = {}
         self.cell_offsets = [(-1,1),(0,1),(1,1),(-1,0),(0,0),(1,0),(-1,-1),(0,-1),(1,-1)]
@@ -39,38 +41,41 @@ class App:
 
     def debug(self, p:Particle):
         if p.tagged:
-            pygame.draw.line(self.DISPLAY_SURF, (255, 255, 255), p.position, p.position + p.velocity, 2)
-            pygame.draw.circle(self.DISPLAY_SURF, (255,255,255), p.position, self.smoothing_radius, 2)
-            p_and_v = f"position: {p.position} velocity: {p.velocity}"
+            pygame.draw.line(self.DISPLAY_SURF, (255, 255, 255), (p.x, p.y), (p.x + p.velocity_x, p.y + p.velocity_y), 2)
+            pygame.draw.circle(self.DISPLAY_SURF, (255,255,255), (p.x, p.y), self.smoothing_radius, 2)
+            p_and_v = f"position: {(round(p.x), round(p.y))} velocity: {(p.velocity_x, p.velocity_y)}"
             p_and_v = self.font(p_and_v)
             self.DISPLAY_SURF.blit(p_and_v,(self.width/3, 10))
-        temp_position = p.position - self.bounds_position
-        pygame.draw.circle(self.DISPLAY_SURF, (255,0,0), temp_position, p.radius)
+        x = p.x - self.bounds_x
+        y = p.y - self.bounds_y
+        pygame.draw.circle(self.DISPLAY_SURF, (255,0,0), (x,y), p.radius)
 
     def move_bounds(self):
         key = pygame.key.get_pressed()
 
         if key[pygame.K_m] and not self.is_bounds_unlocked:
             self.is_bounds_unlocked = True
-            pygame.mouse.set_pos(self.bounds_position)
+            pygame.mouse.set_pos(self.bounds_x, self.bounds_y)
         elif key[pygame.K_m]:
             self.is_bounds_unlocked = False
 
         if self.is_bounds_unlocked:
             mouse_pos = pygame.mouse.get_pos()
-            self.bounds_position = pygame.math.Vector2(mouse_pos)
+            self.bounds_x = mouse_pos[0]
+            self.bounds_y = mouse_pos[1]
 
-    def draw_cell(self, position):
-        cell = self.position_to_cell_coord(position)
-        area = pygame.Rect(cell.x*self.smoothing_radius, cell.y*self.smoothing_radius, self.smoothing_radius, self.smoothing_radius)
+    def draw_cell(self, x, y):
+        cell = self.position_to_cell_coord(x, y)
+        area = pygame.Rect(cell[0]*self.smoothing_radius, cell[1]*self.smoothing_radius, self.smoothing_radius, self.smoothing_radius)
         pygame.draw.rect(self.DISPLAY_SURF, (255,255,255), area, 1)
         
 
     def create_cell_coords(self):
-        num_of_cells = self.bounds / self.smoothing_radius
-        for x in range(math.floor(num_of_cells.x)):
-            for y in range(math.floor(num_of_cells.y)):
-                self.particle_cells[self.hash_cell_key(pygame.math.Vector2(x,y))] = []
+        num_of_cells_x = self.bounds_width / self.smoothing_radius
+        num_of_cells_y = self.bounds_height / self.smoothing_radius
+        for x in range(math.floor(num_of_cells_x)):
+            for y in range(math.floor(num_of_cells_y)):
+                self.particle_cells[self.hash_cell_key(x,y)] = []
 
         threads = []
         for p in self.particals:
@@ -84,52 +89,62 @@ class App:
             t.join()
 
     def foreach_point_within_radius(self, particle: Particle):
-        origin_cell = self.position_to_cell_coord(particle.predicted_position)
+        origin_cell = self.position_to_cell_coord(particle.predicted_x, particle.predicted_y)
         sqr_radius = self.smoothing_radius * self.smoothing_radius
 
         for i in range(9):
-            key = origin_cell + self.cell_offsets[i]
+            key_x = origin_cell[0] + self.cell_offsets[i][0]
+            key_y = origin_cell[1] + self.cell_offsets[i][1]
 
             try:
-                particals = self.particle_cells[self.hash_cell_key(key)]
+                particals = self.particle_cells[self.hash_cell_key(key_x, key_y)]
             except:
                 continue
 
             for other_particle in particals:
+                if particle.tagged:
+                    self.draw_cell(other_particle.x, other_particle.y)
 
-                sqd_dis = (other_particle.predicted_position - particle.predicted_position).magnitude_squared()
+                sqd_dis = self.magnitude_squared(other_particle.predicted_x - particle.predicted_x, other_particle.predicted_y - particle.predicted_y)
                 if (sqd_dis <= sqr_radius):
                     particle.density += self.calculate_density(particle, other_particle)
 
-            pressure_force = pygame.math.Vector2(0,0)
+            pressure_force_x = 0
+            pressure_force_y = 0
 
             for other_particle in particals:
                     
-                sqd_dis = (other_particle.predicted_position - particle.predicted_position).magnitude_squared()
+                sqd_dis = self.magnitude_squared(other_particle.predicted_x - particle.predicted_x, other_particle.predicted_y - particle.predicted_y)
 
                 if (sqd_dis <= sqr_radius):
-                    pressure_force += self.calculate_pressure_force(particle, other_particle)
+                    pressure_forces = self.calculate_pressure_force(particle, other_particle)
+                    pressure_force_x += pressure_forces[0]
+                    pressure_force_y += pressure_forces[1]
 
-            pressure_accelaration = pressure_force / particle.density
-            particle.velocity += pressure_accelaration
+            pressure_accelaration_x = pressure_force_x / particle.density
+            pressure_accelaration_y = pressure_force_y / particle.density
+            particle.velocity_x += pressure_accelaration_x
+            particle.velocity_y += pressure_accelaration_y
         
     def update_cells(self, p:Particle):
         #predict where the partical will go for better reaction
-        p.predicted_position = p.position + p.velocity
-        cell = self.position_to_cell_coord(p.predicted_position)
+        p.predicted_x = p.x + p.velocity_x
+        p.predicted_y = p.y + p.velocity_y
+        cell = self.position_to_cell_coord(p.predicted_x, p.predicted_y)
         try:
-            self.particle_cells[self.hash_cell_key(cell)].append(p)
+            self.particle_cells[self.hash_cell_key(cell[0], cell[1])].append(p)
         except:
-            cell = self.position_to_cell_coord(p.position)
-            self.particle_cells[self.hash_cell_key(cell)].append(p)
+            cell = self.position_to_cell_coord(p.x, p.y)
+            self.particle_cells[self.hash_cell_key(cell[0], cell[1])].append(p)
 
-    def position_to_cell_coord(self, point: pygame.math.Vector2):
-        div_vector = (point - self.bounds_position) / self.smoothing_radius
-        return pygame.math.Vector2(math.floor(div_vector.x), math.floor(div_vector.y))
+    def position_to_cell_coord(self, x, y):
+        div_x = (x - self.bounds_x) / self.smoothing_radius
+        div_y = (y - self.bounds_y) / self.smoothing_radius
+        return math.floor(div_x), math.floor(div_y)
     
-    def hash_cell_key(self, coords:pygame.math.Vector2):
-        a = coords.x * 37
-        b = coords.y * 907
+    def hash_cell_key(self, coord_x, coord_y):
+        a = coord_x * 37
+        b = coord_y * 907
         return a + b
 
     def convert_density_to_pressure(self, density):
@@ -142,15 +157,24 @@ class App:
         pressure_b = self.convert_density_to_pressure(density_b)
         return (pressure_a + pressure_b) / 2
     
+    def magnitude(self, x, y):
+        return math.sqrt(x**2 + y**2)
+    
+    def magnitude_squared(self, x, y):
+        return x**2 + y**2
+    
     def  calculate_pressure_force(self, particle: Particle, other_particle: Particle):
-        if particle.position == other_particle.position:
-            return pygame.math.Vector2(0,0)
+        if particle.x == other_particle.x and particle.y == other_particle.y:
+            return 0, 0
 
-        dst = (other_particle.predicted_position - particle.predicted_position).magnitude()
-        dir = (other_particle.predicted_position - particle.predicted_position) / dst
+        dst = self.magnitude(other_particle.predicted_x - particle.predicted_x, other_particle.predicted_y - particle.predicted_y)
+        dir_x = (other_particle.predicted_x - particle.predicted_x) / dst
+        dir_y = (other_particle.predicted_y - particle.predicted_y) / dst
         slope = self.smoothing_kernal_derivative(dst)
         shared_pressure = self.calculate_shared_pressure(particle.density, other_particle.density)
-        return -shared_pressure * dir * slope * self.MASS / other_particle.density
+        x = -shared_pressure * dir_x * slope * self.MASS / other_particle.density
+        y = -shared_pressure * dir_y * slope * self.MASS / other_particle.density
+        return x, y#, -shared_pressure * dir_y * slope * self.MASS / other_particle.density
 
     def smoothing_kernal(self, dst):
         if dst >= self.smoothing_radius:
@@ -158,7 +182,7 @@ class App:
         volume = (math.pi * pow(self.smoothing_radius, 4)) / 6
         return (self.smoothing_radius - dst) * (self.smoothing_radius - dst) / volume
     
-    def smoothing_kernal_derivative(self, dst):
+    def smoothing_kernal_derivative(self, dst:float):
         if dst >= self.smoothing_radius:
             return 0
         scale = 12 / (pow(self.smoothing_radius, 4) * math.pi)
@@ -166,7 +190,7 @@ class App:
     
     def calculate_density(self, particle: Particle, other_particle: Particle):
         density = 0
-        dst = (other_particle.predicted_position - particle.predicted_position).magnitude()
+        dst = self.magnitude(other_particle.predicted_x - particle.predicted_x, other_particle.predicted_y - particle.predicted_y)
         influence = self.smoothing_kernal(dst)
         density += self.MASS * influence
         return density
@@ -176,8 +200,8 @@ class App:
         particles_per_row, Particles_per_colum = self.particle_start_pos()
         for i, p in enumerate(range(self.num_particles)):
             p = Particle()
-            p.position.x = ((i % particles_per_row + particles_per_row / 4 + 0.5) * (p.radius * 2 + self.spacing)) + self.bounds_position.x
-            p.position.y = ((i / particles_per_row + Particles_per_colum / 4 + 0.5) * (p.radius * 2 + self.spacing)) + self.bounds_position.y
+            p.x = ((i % particles_per_row + particles_per_row / 4 + 0.5) * (p.radius * 2 + self.spacing)) + self.bounds_x
+            p.y = ((i / particles_per_row + Particles_per_colum / 4 + 0.5) * (p.radius * 2 + self.spacing)) + self.bounds_y
             particles.append(p)
         return particles
     
@@ -187,7 +211,7 @@ class App:
         return particles_per_row, Particles_per_colum 
     
     def update_partical(self, p: Particle):
-        p.velocity += pygame.math.Vector2(0,1) * self.gravity
+        p.velocity_y += self.gravity
 
         if p.tagged:
             p.color = (0,255,0)
@@ -197,13 +221,13 @@ class App:
         self.foreach_point_within_radius(p)
         
         p.update_velocity()
-        p.update_collisions(self.bounds, self.bounds_position)
+        p.update_collisions(self.bounds_width, self.bounds_height, self.bounds_x, self.bounds_y)
         p.draw(self.DISPLAY_SURF)
 
         self.debug(p)
 
     def draw_bounds(self):
-        pygame.draw.rect(self.DISPLAY_SURF, (255,255,255), (self.bounds_position, self.bounds), 1)
+        pygame.draw.rect(self.DISPLAY_SURF, (255,255,255), ((self.bounds_x, self.bounds_y), (self.bounds_width, self.bounds_height)), 1)
             
     def on_quit(self):
         pygame.quit()
@@ -216,7 +240,7 @@ class App:
     def fps_counter(self, clock):
         fps = str(int(clock.get_fps()))
         fps_t = self.font(fps)
-        self.DISPLAY_SURF.blit(fps_t,(self.bounds.x + 40,0))
+        self.DISPLAY_SURF.blit(fps_t,(self.bounds_x + 40,0))
  
     def on_execute(self):
         clock = pygame.time.Clock()
--- fluid-sim-test/particle.py
+++ fluid-sim-test/particle.py
@@ -4,9 +4,12 @@ import math
 
 class Particle:
     def __init__(self):
-        self.position = pygame.math.Vector2(0, 0)
-        self.predicted_position = pygame.math.Vector2(0, 0)
-        self.velocity = pygame.math.Vector2(0, 0)
+        self.x = 0
+        self.y = 0
+        self.predicted_x = 0
+        self.predicted_y = 0
+        self.velocity_x = 0
+        self.velocity_y = 0
         self.density = 1
         self.color = (0,0,255)
         self.radius = 5
@@ -15,18 +18,21 @@ class Particle:
         self.tagged = False
 
     def update_velocity(self):
-        self.position += self.velocity
+        self.x += self.velocity_x
+        self.y += self.velocity_y
 
-    def update_collisions(self, bounds: pygame.math.Vector2, bounds_position: pygame.math.Vector2):
-        bounds_radius = bounds - pygame.math.Vector2(self.radius, self.radius)
-        temp_position = self.position - bounds_position
+    def update_collisions(self, bounds_width, bounds_height, bounds_x, bounds_y):
+        bounds_radius_w = bounds_width - self.radius
+        bounds_radius_h = bounds_height - self.radius
+        detect_x = self.x - bounds_x
+        detect_y = self.y - bounds_y
 
-        if temp_position.x > bounds_radius.x or temp_position.x < 0:
-            self.position.x = (bounds_radius.x * self.sign(temp_position.x)) + bounds_position.x
-            self.velocity.x *= -1 * self.collison_damping
-        if temp_position.y > bounds_radius.y or temp_position.y < 0:
-            self.position.y = (bounds_radius.y * self.sign(temp_position.y)) + bounds_position.y
-            self.velocity.y *= -1 * self.collison_damping
+        if detect_x > bounds_radius_w or detect_x < 0:
+            self.x = (bounds_radius_w * self.sign(detect_x)) + bounds_x
+            self.velocity_x *= -1 * self.collison_damping
+        if detect_y > bounds_radius_h or detect_y < 0:
+            self.y = (bounds_radius_h * self.sign(detect_y)) + bounds_y
+            self.velocity_y *= -1 * self.collison_damping
 
     def sign(self, number):
         #the display starts at 0 and does not go negative
@@ -37,5 +43,5 @@ class Particle:
 
 
     def draw(self, surface):
-        pygame.draw.circle(surface, self.color, self.position, self.radius)
+        pygame.draw.circle(surface, self.color, (self.x, self.y), self.radius)
         
